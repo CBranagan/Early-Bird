6b1eebc1a537aa01f272992c43850087c1579142
Chad Branagan <chad.branagan@gmail.com>
2021-10-27 19:01:11 -0700

    fix again

--- assets/jss/indexScript.js
+++ assets/jss/indexScript.js
@@ -48,6 +48,7 @@ var horoscopeHandler = function(event) {
 		mood: horoMood.checked,
     };
 
+
 	localStorage.setItem("HoroscopeStuff", JSON.stringify(horoDataObj));
     
     
@@ -85,7 +86,8 @@ var jokeWidget = function() {
             var punchLine = data.body[0].punchline
             
             var jokeCard = document.createElement("div");
-            jokeCard.classList = "column";
+            jokeCard.classList = "card";
+            jokeCard.setAttribute("style", "width: 300px")
             
             var jokeSetup = document.createElement("p");
             jokeSetup.textContent = joke;
@@ -114,6 +116,9 @@ var jokeWidget = function() {
 
 var horoscopeWidget = function(horoDataObj) {
 
+    // var userSign = localStorage.getItem("HoroscopeStuff")
+	// var userInfo = JSON.parse(userSign);
+
     var userInfo = localStorage.getItem("HoroscopeStuff")
 
     console.log(userInfo)
@@ -130,30 +135,10 @@ var horoscopeWidget = function(horoDataObj) {
         "headers": {
             "x-rapidapi-host": "sameer-kumar-aztro-v1.p.rapidapi.com",
             "x-rapidapi-key": "5b4f00da92mshdc043b28ff0d6c7p1cab71jsn744cc19d64cb"
-<<<<<<< HEAD
              }
         })
         .then(response => response.json())
         .then(function(data) {
-=======
-        }
-    })
-    .then(response => {
-        response.json().then(function(data) {
-            
-            var horoscopeCard = document.createElement("div");
-<<<<<<< HEAD
-            horoscopeCard.classList = "card"
-            horoscopeCard.setAttribute("style", "width: 300px")
-
-            for (var i=1; i < userInfo2.length; i++) {
-
-                if (userInfo2[i]) {
-
-                    var horoElInfo = horoscopeApiCalls[i-1]
-
-                    var horoEloutput = data.horoElInfo
->>>>>>> 32069b66a71af49bd6323b9ed0fd42e859796385
 
             console.log(data)
                 
@@ -179,9 +164,6 @@ var horoscopeWidget = function(horoDataObj) {
                         widgets.appendChild(horoscopeCard)
                         horoscopeCard.appendChild(horoEl)
 
-=======
-
->>>>>>> 0ed36590e203e4f4a22338c74850e9715cdb7eb0
             
         })
         .catch(err => {
